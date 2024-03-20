1072b1ba3a7be3da5426d9cd5337be4c559539e5
manuellalonzo <66815385+manuellalonzo@users.noreply.github.com>
2024-03-20 08:46:37 -0700

    Update output.tf
    updated the wait 120 seconds and then run curl command for hello world website

--- demo1/output.tf
+++ demo1/output.tf
@@ -1,3 +1,18 @@
 output "lb_ip" {
   value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.hostname
 }
+
+resource "time_sleep" "wait_120_seconds" {
+  create_duration = "120s"
+}
+
+
+resource "null_resource" "curl_hello_world" {
+
+  # shows output of curl command for load balancer
+ provisioner "local-exec" {
+
+    command = "curl -L http://${kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.hostname}:12344"
+  }
+  depends_on = [time_sleep.wait_120_seconds]
+}
